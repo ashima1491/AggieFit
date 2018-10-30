0d880edfa2a745d0bc2c673bb99576016b3ad363
Nikhil Gupta <niksgupta36@gmail.com>
2018-10-30 14:58:23 -0500

    refactor code

--- message-board.py
+++ message-board.py
@@ -1,29 +1,17 @@
-'''
-Created on Oct 28, 2018
-
-@author: sharm
-'''
-'''
-Created on Oct 28, 2018
-
-@author: sharm
-'''
+import threading
+import time
 import redis
 import database_crud
+from time import sleep
 dir(redis)
 
 r = redis.Redis()
 
 
 subscribing = False;
-topic = "";
-
+channel="";
 while True:
     try:
-#         if subscribing:
-#             print("Sub")
-#             for item in p.listen():    
-#                 print(item)
 
         cmd = input('Enter your command: ')
         print(cmd)
@@ -34,33 +22,57 @@ while True:
             if(""==channel):
                print("Enter a message board to select ") 
         elif cmd_parts[0] == "read":
-            database_crud.getRecords(channel)
+            if(""==channel):
+                print("Enter a message board to read ") 
+            if(channel!=""):    
+                database_crud.getRecords(channel)
            
         elif cmd_parts[0] == "write":
             to_pub = ' '.join(cmd_parts[1:])
+            if(channel==""):
+               print("Enter a message board to write ")
+                
             res = r.publish(channel, to_pub) 
             print(res)
-            database_crud.insertRecords(channel, cmd_parts[1:])
+            if(channel!=""):
+                database_crud.insertRecords(channel, cmd_parts[1:])
         elif cmd_parts[0] == "listen":
-            subscribing = True;
-            p = r.pubsub()
-            res = p.subscribe([channel]) 
-            print(res)
+            if(""==channel):
+                print("Enter a message board to listen ")
+            else: 
+                subscribing = True;
+                p = r.pubsub()
+                res = p.subscribe([channel])
+                if subscribing:
+                    print("Sub")
+                    try:    
+                        temp = p.listen()
+                        for item in temp: 
+                            print(item)
+                    except KeyboardInterrupt:
+                        p.unsubscribe([channel])
+                        break;
+                print(res)
         elif cmd_parts[0] == "stop":
-            subscribing = False;
-            res = p.unsubscribe([channel]) 
-            print(res)    
+            if(""==channel):
+                print("Enter a message board to stop ")
+            elif subscribing == False:
+                print("Not listening to any channel")    
+            else:    
+                subscribing = False;
+                p = r.pubsub()
+                res = p.unsubscribe([channel]) 
+                print(res)    
         elif cmd_parts[0] == "quit":
             break;
         else:
             print("Input format wrong");
-            
-        if subscribing:
-            print("Sub")
-            for item in p.listen():    
-                print(item)
+                    
+           
     except KeyboardInterrupt:
+        
         subscribing = False
+        break;
+        
 
-
-    
+        
--- mongo_connect.py
+++ mongo_connect.py
@@ -1,12 +1,6 @@
 import pymongo
 import constants
 
-# def connectMongo():
-# 	url = "mongodb://" + constants.host + "/" + constants.db
-# 	client = pymongo.MongoClient(url)
-# 	db = client[constants.db]
-# 	collection = db[constants.collection]
-# 	return collection
 
 def connectMongo():
     url = 'mongodb+srv://admin:admin@cluster0-v0u4h.mongodb.net/test?retryWrites=true'
